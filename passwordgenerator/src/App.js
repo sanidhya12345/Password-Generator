4802524d2df08b0535447175c920dea4f18c6dcc
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-11-08 16:36:33 +0530

    react

--- passwordgenerator/src/App.js
+++ passwordgenerator/src/App.js
@@ -1,25 +1,40 @@
-import './App.css'
-import { useState } from 'react'
-export const App=()=>{
-    const [fullname,setName]=useState('');
-    const handleName=(e)=>{
-        e.preventDefault();
-        setName(e.target.value);
+/** @format */
+
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    fullname: "",
+    click: "",
+  };
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "fullname") {
+      this.setState({ [name]: value });
+    } else if (name === "click") {
+      let click = this.state.click === "on" ? "off" : "on";
+      this.setState({ click });
     }
-    const [click,setClick]=useState("false");
-    const handleClick=(e)=>{
-        e.target.value==='on'?setClick("true"):setClick("false");
-    }
-    return(
-        <div>
-            <h1>Password Generator</h1>
-            <div className='name'>
-                <label htmlFor=''>Your Name </label>
-                <input type='text' name='fullname' onChange={handleName}></input>
-                <label htmlFor=''>Click Me</label>
-                <input type='checkbox' name='click' onChange={handleClick}></input>
-                <h1>{click}</h1>
-            </div>
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <h1>Password Generator</h1>
+        <div className="name">
+          <label htmlFor="">Your Name </label>
+          <input
+            type="text"
+            name="fullname"
+            onChange={this.handleChange}></input>
         </div>
-    )
+        <div>
+            
+        </div>
+      </div>
+    );
+  }
 }
+
+export default App;
