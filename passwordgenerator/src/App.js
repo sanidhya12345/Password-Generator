914d00c0913b2197e993031e534b6d4a06e27926
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2023-11-08 16:53:30 +0530

    react

--- passwordgenerator/src/App.js
+++ passwordgenerator/src/App.js
@@ -6,15 +6,30 @@ import React, { Component } from "react";
 class App extends Component {
   state = {
     fullname: "",
-    click: "",
+    number: "off",
+    lcl:"off",
+    ucl:"off",
+    sym:"off"
   };
   handleChange = (e) => {
     const { name, value } = e.target;
     if (name === "fullname") {
       this.setState({ [name]: value });
-    } else if (name === "click") {
-      let click = this.state.click === "on" ? "off" : "on";
-      this.setState({ click });
+    } else if (name === "number") {
+      let number = this.state.number === "on" ? "off" : "on";
+      this.setState({ number });
+    }
+    else if(name==="lcl"){
+        let lcl = this.state.lcl === "on" ? "off" : "on";
+        this.setState({ lcl });
+    }
+    else if(name==="ucl"){
+        let ucl = this.state.ucl === "on" ? "off" : "on";
+        this.setState({ ucl });
+    }
+    else if(name==="sym"){
+        let sym = this.state.sym === "on" ? "off" : "on";
+        this.setState({ sym });
     }
   };
   render() {
@@ -29,8 +44,35 @@ class App extends Component {
             name="fullname"
             onChange={this.handleChange}></input>
         </div>
-        <div>
-            
+        <div className="include">
+          <div>
+            <label htmlFor="">Numbers</label>
+            <input
+              type="checkbox"
+              name="number"
+              onChange={this.handleChange}></input>
+          </div>
+          <div>
+            <label htmlFor="">Lower Case Letters</label>
+            <input
+              type="checkbox"
+              name="lcl"
+              onChange={this.handleChange}></input>
+          </div>
+          <div>
+            <label htmlFor="">Upper Case Letters</label>
+            <input
+              type="checkbox"
+              name="ucl"
+              onChange={this.handleChange}></input>
+          </div>
+          <div>
+            <label htmlFor="">Symbol</label>
+            <input
+              type="checkbox"
+              name="sym"
+              onChange={this.handleChange}></input>
+          </div>
         </div>
       </div>
     );
